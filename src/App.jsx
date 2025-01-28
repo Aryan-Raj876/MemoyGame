d35499d035411a9a6ff7c1860ee022c80ccdfa73
Aryan-Raj876 <aryanrajgit876@gmail.com>
2025-01-28 13:26:02 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import GameBoard from "./Game/GameBoard"; 
+
+
+function App() {
+
+  return (
+    <>
+      <div className="App"> 
+            <GameBoard /> 
+        </div> 
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Game/Data.jsx
@@ -0,0 +1,82 @@
+import cir from "../assets/game.png";
+import poker from "../assets/poker.png";
+import gammon from "../assets/backgammon.png";
+import board from "../assets/boardgame.png";
+import pur from "../assets/purpcir.png";
+import lud from "../assets/ludo.png";
+  
+const Data = [ 
+    { 
+        id: 1, 
+        name: "pokeball", 
+        img:  cir,
+        matched: false, 
+    }, 
+    { 
+        id: 2, 
+        name: "ludo", 
+        img:  lud,
+        matched: false, 
+    }, 
+    { 
+        id: 3, 
+        name: "boardgame", 
+        img:  board,
+        matched: false, 
+    }, 
+    { 
+        id: 4, 
+        name: "backgammon", 
+        img:  gammon,
+        matched: false, 
+    }, 
+    { 
+        id: 5, 
+        name: "purp", 
+        img:  pur,
+        matched: false, 
+    }, 
+    { 
+        id: 6, 
+        name: "puker", 
+        img:  poker,
+        matched: false, 
+    }, 
+    { 
+        id: 7, 
+        name: "pokeball", 
+        img:  cir,
+        matched: false, 
+    }, 
+    { 
+        id: 8, 
+        name: "ludo", 
+        img:  lud,
+        matched: false, 
+    }, 
+    { 
+        id: 9, 
+        name: "boardgame", 
+        img: board,  
+        matched: false, 
+    }, 
+    { 
+        id: 10, 
+        name: "backgammon", 
+        img:  gammon,
+        matched: false, 
+    }, 
+    { 
+        id: 11, 
+        name: "purp", 
+        img:  pur,
+        matched: false, 
+    }, 
+    { 
+        id: 12, 
+        name: "puker", 
+        img:  poker, 
+        matched: false, 
+    }, 
+]; 
+export default Data;
